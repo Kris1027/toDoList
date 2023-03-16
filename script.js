a875ece8db713bbc4a18e17d524e1b3d0346617e
Kris <120410235+Kris1027@users.noreply.github.com>
2023-03-17 00:02:48 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const addToDoButton = document.getElementById('addToDoButton');
-const toDoContainer = document.getElementById('toDoContainer');
-const inputField = document.getElementById('inputField');
+const addToDoButton = document.getElementById('addToDoButton');     //
+const toDoContainer = document.getElementById('toDoContainer');    // Podpinam elementy DOM - button, input i kontener na paragrafy
+const inputField = document.getElementById('inputField');         //
 
-addToDoButton.addEventListener('click', function()
+addToDoButton.addEventListener('click', function()   // Dodaje do button metodę działania po kliknięciu i niemą funkcję
 {
-    const text = inputField.value;
-    const newParagraph = document.createElement('p');
-    const paragraphText = document.createTextNode(text);
-    newParagraph.appendChild(paragraphText);
-    toDoContainer.appendChild(newParagraph);
-    newParagraph.classList.add('p-style')
-    inputField.value = '';
+    const text = inputField.value;   // Tworzę zmienną która pobiera wartość z input
+    const newParagraph = document.createElement('p');   // Tworzę zmienną która tworzy paragraf
+    const paragraphText = document.createTextNode(text);   // Tworze zmienną która pozwala na wpisanie tekstu dzięki metodzie createTextNode
+    newParagraph.appendChild(paragraphText);   // Dodaje do zmiennej nowego paragrafu zmienna z tekstem
+    toDoContainer.appendChild(newParagraph);   // Dodaje do kontenera na paragrafy miejsce na nowe paragrafy
+    newParagraph.classList.add('p-style')   // Dodaje klase do paragrafow
+    inputField.value = '';   // Dodaje wyczyszczenie pola input, kiedy dodam paragraf
     newParagraph.addEventListener('click', function() 
     {
-        newParagraph.style.textDecoration = 'line-through';
+        newParagraph.style.textDecoration = 'line-through';   // Dodaje przekreślenie tekstu
     })
-    newParagraph.addEventListener('dblclick', function()
+    newParagraph.addEventListener('dblclick', function()  // Poprzednio było jedno kliknięcie, dodanie "dbl" oznacza dwa kliknięcia
     {
-        toDoContainer.removeChild(newParagraph);
+        toDoContainer.removeChild(newParagraph);   // Usuwa paragraf
     })
 })
